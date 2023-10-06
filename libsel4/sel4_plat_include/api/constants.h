2adcfa51be7068543375fb5c884fe5737dc2ad2f
Petra Heeb <petra.heeb1@ibm.com>
2023-10-06 14:08:05 +0200

    update sel4 kernel with starfive platform"
    Signed-off-by: Petra Heeb <petra.heeb1@ibm.com>

---
+++ libsel4/sel4_plat_include/api/constants.h
@@ -0,0 +1,10 @@
+/*
+ * Copyright 2020, Data61, CSIRO (ABN 41 687 119 230)
+ * Copyright 2023, IBM
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#pragma once
+
+#include <sel4/config.h>
---
+++ src/plat/starfive/config.cmake
@@ -0,0 +1,24 @@
+#
+# Copyright 2023, IBM
+#
+# SPDX-License-Identifier: GPL-2.0-only
+#
+cmake_minimum_required(VERSION 3.7.2)
+
+declare_platform(starfive KernelPlatformStarfive PLAT_STARFIVE KernelSel4ArchRiscV64)
+
+if(KernelPlatformStarfive)
+    declare_seL4_arch(riscv64)
+    config_set(KernelRiscVPlatform RISCV_PLAT "starfive")
+    config_set(KernelPlatformFirstHartID FIRST_HART_ID 1)
+    config_set(KernelOpenSBIPlatform OPENSBI_PLATFORM "generic")
+    set(KernelRiscvUseClintMtime ON)
+    list(APPEND KernelDTSList "tools/dts/starfive.dts")
+    list(APPEND KernelDTSList "src/plat/starfive/overlay-starfive.dts")
+    declare_default_headers(
+        TIMER_FREQUENCY 1000000 PLIC_MAX_NUM_INT 53
+        INTERRUPT_CONTROLLER drivers/irq/riscv_plic0.h
+    )
+else()
+    unset(KernelPlatformFirstHartID CACHE)
+endif()
